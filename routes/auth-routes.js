23c578767ff4b2f2824ebf7ce675e449767d65b8
Emmanuel-kiplimo <kiplimokemei0@gmail.com>
2025-06-15 11:27:26 +0300

    otp changes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {register, login, verifyEmail} = require('../controllers/usercontroller');
+const {register, login, verifyOtp} = require('../controllers/usercontroller'); // Changed verifyEmail to verifyOtp
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/verify-otp', verifyOtp);
+router.post('/verify-otp', verifyOtp); // This now correctly refers to the imported verifyOtp
 
 
 module.exports = router;
